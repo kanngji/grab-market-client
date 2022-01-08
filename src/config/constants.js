6d685834b5167ebf5af6834b6ce021ab32fa688d
kangji <alleneffort1994@gmail.com>
2022-01-08 17:37:57 +0900

    api 주소변경

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,1 +1,4 @@
-export const API_URL = "http://localhost:8080";
+export const API_URL =
+  process.env.NODE_ENV === "production"
+    ? "https://kangji-market-server.herokuapp.com"
+    : "https://localhost:8080";
